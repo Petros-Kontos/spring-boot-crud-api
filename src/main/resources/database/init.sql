8bef4898a35f8d18b82078c86b77ed2423c16fa1
Petros Kontos <p_kontos@hotmail.com>
2021-05-07 14:17:31 +0300

    init.sql payment_details

--- src/main/resources/database/init.sql
+++ src/main/resources/database/init.sql
@@ -4,18 +4,35 @@ CREATE DATABASE  IF NOT EXISTS `payroll_directory`;
 USE `payroll_directory`;
 
 DROP TABLE IF EXISTS `employee`;
+DROP TABLE IF EXISTS `payment_details`;
+
+CREATE TABLE `payment_details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `account_number` varchar(20) NOT NULL,
+  `sort_code` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 CREATE TABLE `employee` (
   `id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
+  `payment_details_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (payment_details_id) REFERENCES payment_details(id)
 );
 
+INSERT INTO `payment_details` VALUES
+	(1, 23795837, 592379324),
+	(2, 0239875098, 9759558),
+	(3, 987798686, 8595675),
+	(4, 769769867, 985965865),
+	(5, 659565, 87957868);
+
 INSERT INTO `employee` VALUES
-	(1,'Alex','Turner','alex@arcticmonkeys.com'),
-	(2,'Axl','Rose','axl@gunsnroses.com'),
-	(3,'Bruce','Dickinson','bruce@ironmaiden.com'),
-	(4,'Kurt','Cobain','kurt@nirvana.com'),
-	(5,'James','Hetfield','james@metallica.com');
+	(1,'Alex','Turner','alex@arcticmonkeys.com', 1),
+	(2,'Axl','Rose','axl@gunsnroses.com', 2),
+	(3,'Bruce','Dickinson','bruce@ironmaiden.com', 3),
+	(4,'Kurt','Cobain','kurt@nirvana.com', 4),
+	(5,'James','Hetfield','james@metallica.com', 5);
